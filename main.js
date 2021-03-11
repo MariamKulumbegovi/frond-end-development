f910049a72606cb3e04c3e179dc82199144f7bef
MariamKulumbegovi <mweaving7@gmail.com>
2021-03-11 15:41:26 +0400

    add 2 assignments

---
+++ main.js
@@ -0,0 +1,30 @@
+var x=18
+var y=-43
+var c=x+y
+
+if(c%2===0) {
+    console.log('c is even')
+}else if(c%2===1){
+    console.log('c is odd')
+}else if(c%2===-1){
+    console.log('c is odd')
+}else{
+    console.log('c is unknown type')
+}
+
+var b=-21
+var h=7
+var a=1/2*b*h
+
+if(a>0){
+    console.log(a)
+}else if(a<0){
+    console.log(-a)
+}else{
+    console.log('a is unknown type')
+}
+
+
+
+
+
