2b9d74a32dd6f4e69862fa769e96650dc97f4d9c
MariamKulumbegovi <mweaving7@gmail.com>
2021-03-15 00:00:20 +0400

    add 2 tasks

---
+++ main.js
@@ -0,0 +1,97 @@
+// var number=1
+
+// while(number<100){
+//     console.log(number)
+//     number+=1
+// }
+
+// while(number<=30){
+//     if(number%2===0){
+//         console.log(number)
+//     }
+//     number=number+1
+// }
+
+// while(number<20){
+//     if(number===10){
+//         break
+//     }
+//     console.log(number)
+//     number=number+1
+// }
+
+
+// function mari(x,y) {
+//     return x+y
+
+// }
+
+// // var j=mari(50,10)
+// console.log(mari(10,20))
+
+
+// var r=function(x,y){
+//     return x/y
+// }
+
+// var i=r(21,7)
+// console.log(i)
+
+// var h=(x,y) => {
+//     return x-y
+// }
+
+// var l=h(70,10)
+// console.log(l)
+
+
+// x=1
+
+// function bun(x){
+//     if(x%2===1){
+//         return true
+
+//     }
+//     return false
+
+// }
+// for(var i=20; i>0; i--){
+//     console.log('index', i, bun(i))
+// }
+
+// function rose(x=0){
+//     if(x>10){
+//         return x
+
+//     }
+//     console.log(x)
+//     return rose(x+1)
+    
+// }
+// task1
+
+var title="Smartacademy"
+
+for(var i=title.length; i>=0; i--){
+    console.log('index', title[i])
+    
+}
+// task1
+
+// task2
+
+var numbers = '123456789' 
+
+for(var i=0; i<numbers.length; i++){
+    console.log('index', numbers[i]**numbers[i])
+}
+
+// task2
+
+// task3
+
+// var numbers = '123456789' 
+
+// for(var i=0; i<numbers.length; i++){
+//     console.log('index', numbers[i])
+// }
