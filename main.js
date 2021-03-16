74cab254b29ce1628e97d008f47529f553cbb29e
MariamKulumbegovi <mweaving7@gmail.com>
2021-03-16 11:58:08 +0400

    Update main.js

--- main.js
+++ main.js
@@ -82,6 +82,7 @@ for(var i=title.length; i>=0; i--){
 
 var numbers = '123456789' 
 
+
 for(var i=0; i<numbers.length; i++){
     console.log('index', numbers[i]**numbers[i])
 }
@@ -90,8 +91,14 @@ for(var i=0; i<numbers.length; i++){
 
 // task3
 
-// var numbers = '123456789' 
 
-// for(var i=0; i<numbers.length; i++){
-//     console.log('index', numbers[i])
-// }
+
+let startingnum = "123456789"
+var numbers = startingnum.split``.map(x=>+x);
+for(j=0; j <= numbers.length-1; j++) {
+    for(g=1; g <= numbers.length; g++){
+     console.log(Math.pow(numbers[j],g))
+    }
+}
+
+// task3
