6f8e602f6f72883a77037f3ec3e6c5c15773a0dc
MariamKulumbegovi <mweaving7@gmail.com>
2021-03-21 23:32:39 +0400

    add buttons

---
+++ main.js
@@ -0,0 +1,32 @@
+var list=[]
+
+
+var nameinput=document.getElementById('thenameinput')
+
+function nameinputer() {
+    if ((list.push(thenameinput.value))){
+        console.log(list)
+    }
+        
+    
+}
+
+
+// var products=document.getElementById('productsul')
+
+
+
+
+var priceinput=document.getElementById('thepriceinput')
+
+var total=[]
+
+function priceinputer() {
+    if((total.push(thepriceinput.value,))){
+    console.log(total)
+
+    }
+    
+
+    
+}
